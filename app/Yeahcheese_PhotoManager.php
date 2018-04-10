285c04f82791a4a4e89a08f3890a280ac86c92ef
mikankari <Endo@MAC18L10G0267.local>
2018-04-10 11:14:20 +0900

    空の写真ロジックを追加

---
+++ app/Yeahcheese_PhotoManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Yeahcheese_PhotoManager.php
+ *
+ * @author {$author}
+ * @package Yeahcheese
+ */
+
+/**
+ * Yeahcheese_PhotoManager
+ *
+ * @author {$author}
+ * @access public
+ * @package Yeahcheese
+ */
+class Yeahcheese_PhotoManager extends Ethna_AppManager
+{
+}
+?>
