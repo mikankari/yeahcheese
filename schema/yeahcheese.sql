ed0d8cca5d01893cc85053e698696e71da69342b
mikankari <Endo@MAC18L10G0267.local>
2018-04-10 09:24:25 +0900

    データベース設計SQLを追加

---
+++ schema/yeahcheese.sql
@@ -0,0 +1,22 @@
+
+create table users (
+    id      serial  primary key,
+    name    text    not null,
+    mail    text    not null    unique,
+    hash    text    not null
+);
+
+create table events (
+    id              serial      primary key,
+    user_id         int         not null,
+    name            text        not null,
+    hash            text        not null    unique,
+    publish_at      timestamp    not null,
+    publish_end_at  timestamp    not null
+);
+
+create table photos (
+    id          serial  primary key,
+    event_id    int     not null,
+    filename    text    not null
+);
