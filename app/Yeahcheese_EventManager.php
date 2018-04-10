c0d72003af2c04d12cd09e245a7a40f9aaead0ed
mikankari <Endo@MAC18L10G0267.local>
2018-04-10 18:38:06 +0900

    イベントロジックに、ユーザごとにイベントを取得するメソッドを追加

--- app/Yeahcheese_EventManager.php
+++ app/Yeahcheese_EventManager.php
@@ -15,5 +15,10 @@
  */
 class Yeahcheese_EventManager extends Ethna_AppManager
 {
+    function getUserEvents($user_id){
+        $events = $this->db->getAll('select * from events where user_id = ' . $user_id);
+
+        return $events;
+    }
 }
 ?>
