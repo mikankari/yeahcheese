78fe1ec96451dabf3472209710254699d13592ec
mikankari <Endo@MAC18L10G0267.local>
2018-04-10 18:04:08 +0900

    空のイベントロジックを作成

---
+++ app/Yeahcheese_EventManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Yeahcheese_EventManager.php
+ *
+ * @author {$author}
+ * @package Yeahcheese
+ */
+
+/**
+ * Yeahcheese_EventManager
+ *
+ * @author {$author}
+ * @access public
+ * @package Yeahcheese
+ */
+class Yeahcheese_EventManager extends Ethna_AppManager
+{
+}
+?>
