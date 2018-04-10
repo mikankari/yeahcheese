d223128f93ff034e912c15f1c57b0e2dbd301a96
mikankari <Endo@MAC18L10G0267.local>
2018-04-10 10:25:43 +0900

    空のイベント閲覧ページを作成

---
+++ app/view/Event/Show.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Event/Show.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+/**
+ *  event_show view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Yeahcheese
+ */
+class Yeahcheese_View_EventShow extends Yeahcheese_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
