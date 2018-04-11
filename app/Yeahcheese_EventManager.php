e031eb1bffeb1d7806a0eaeeb0e4bb893b547387
mikankari <Endo@MAC18L10G0267.local>
2018-04-11 11:32:34 +0900

    イベントロジックに、イベントの追加と編集をするメソッドを追加

--- app/Yeahcheese_EventManager.php
+++ app/Yeahcheese_EventManager.php
@@ -20,5 +20,40 @@ class Yeahcheese_EventManager extends Ethna_AppManager
 
         return $events;
     }
+
+    function addUserEvent($userId, $formVars){
+        $result = $this->db->execute('insert into events (user_id, name, hash, publish_at, publish_end_at) values (?, ?, ?, ?, ?)', [
+            $userId,
+            uniqid(),
+            $formVars['name'],
+            $formVars['publish_at'],
+            $formVars['publish_end_at'],
+        ]);
+        var_dump($this->db->db->errorMsg());
+
+        if(! $result){
+            return false;
+        }
+
+        $insertId = $this->db->getOne('select max(id) from events');
+
+        return $insertId;
+    }
+
+    function editUserEvent($userId, $eventId, $formVars){
+        $result = $this->db->execute('update events set name = ?, publish_at = ?, publish_end_at = ? where id = ? and user_id = ?', [
+            $formVars['name'],
+            $formVars['publish_at'],
+            $formVars['publish_end_at'],
+            $eventId,
+            $userId,
+        ]);
+
+        if(! $result){
+            return false;
+        }
+
+        return $eventId;
+    }
 }
 ?>
