bd68798970fbaab40861089d16381d139961f874
mikankari <Endo@MAC18L10G0267.local>
2018-04-11 15:08:37 +0900

    【修正】コーディング規約により前カンマにする

--- schema/yeahcheese.sql
+++ schema/yeahcheese.sql
@@ -1,21 +1,21 @@
 
 CREATE TABLE users (
-    id      SERIAL  PRIMARY KEY,
-    name    TEXT    NOT NULL,
-    mail    TEXT    NOT NULL    UNIQUE,
-    hash    TEXT    NOT NULL
+        id      SERIAL  PRIMARY KEY
+    ,   name    TEXT    NOT NULL
+    ,   mail    TEXT    NOT NULL    UNIQUE
+    ,   hash    TEXT    NOT NULL
 );
 
 CREATE TABLE events (
-    id              SERIAL      PRIMARY KEY,
-    user_id         INT         NOT NULL,
-    name            TEXT        NOT NULL,
-    hash            TEXT        NOT NULL    UNIQUE,
-    publish_at      TIMESTAMP   NOT NULL,
-    publish_end_at  TIMESTAMP   NOT NULL
+        id              SERIAL      PRIMARY KEY
+    ,   user_id         INT         NOT NULL
+    ,   name            TEXT        NOT NULL
+    ,   hash            TEXT        NOT NULL    UNIQUE
+    ,   publish_at      TIMESTAMP   NOT NULL
+    ,   publish_end_at  TIMESTAMP   NOT NULL
 );
 
 CREATE TABLE photos (
-    id          SERIAL  PRIMARY KEY,
-    event_id    INT     NOT NULL
+        id          SERIAL  PRIMARY KEY
+    ,   event_id    INT     NOT NULL
 );
