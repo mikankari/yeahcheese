451e0e64d2cdce98f3b7acf799322aa0e97ad765
mikankari <endo@sencorp.co.jp>
2018-04-12 10:31:10 +0900

    mail, hash, publish_at のフィールド名を変更

--- schema/yeahcheese.sql
+++ schema/yeahcheese.sql
@@ -1,18 +1,18 @@
 
 CREATE TABLE users (
-        id      SERIAL  PRIMARY KEY
-    ,   name    TEXT    NOT NULL
-    ,   mail    TEXT    NOT NULL    UNIQUE
-    ,   hash    TEXT    NOT NULL
+        id          SERIAL  PRIMARY KEY
+    ,   name        TEXT    NOT NULL
+    ,   email       TEXT    NOT NULL    UNIQUE
+    ,   password    TEXT    NOT NULL
 );
 
 CREATE TABLE events (
-        id              SERIAL      PRIMARY KEY
-    ,   user_id         INT         NOT NULL
-    ,   name            TEXT        NOT NULL
-    ,   hash            TEXT        NOT NULL    UNIQUE
-    ,   publish_at      TIMESTAMP   NOT NULL
-    ,   publish_end_at  TIMESTAMP   NOT NULL
+        id                  SERIAL      PRIMARY KEY
+    ,   user_id             INT         NOT NULL
+    ,   name                TEXT        NOT NULL
+    ,   password            TEXT        NOT NULL    UNIQUE
+    ,   publish_start_at    TIMESTAMP   NOT NULL
+    ,   publish_end_at      TIMESTAMP   NOT NULL
 );
 
 CREATE TABLE photos (
