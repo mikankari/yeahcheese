4c0f53e4558c0dd92d52aaf2248375d28d803d55
mikankari <endo@sencorp.co.jp>
2018-04-12 15:30:29 +0900

    空の共有者ログインページを作成

---
+++ app/action/User/Login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  User/Login.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Action_UserLogin extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return 'user_login';
+    }
+}
---
+++ app/action/User/Login/Execute.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  User/Login/Execute.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Form_UserLoginExecute extends Yeahcheese_ActionForm
+{
+    public $form = [
+        
+    ];
+}
+
+class Yeahcheese_Action_UserLoginExecute extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
