fad07464f948924df09e0baa9774cf56dafcc768
mikankari <endo@sencorp.co.jp>
2018-04-12 17:03:50 +0900

    空の会員登録ページを作成

---
+++ app/action/User/Register/Execute.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  User/Register/Execute.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Form_UserRegisterExecute extends Yeahcheese_ActionForm
+{
+    public $form = [
+
+    ];
+}
+
+class Yeahcheese_Action_UserRegisterExecute extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
