760522967e42fd0d64a8f47634e3eec6d92255c7
mikankari <endo@sencorp.co.jp>
2018-04-12 16:22:11 +0900

    空の共有者ログイン後ページを作成

---
+++ app/action/User/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  User/Index.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Action_UserIndex extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return 'user_index';
+    }
+}
