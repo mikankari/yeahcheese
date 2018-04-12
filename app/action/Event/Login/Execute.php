6998d917cb70d662efc6f21f968c95a669ae49d2
mikankari <endo@sencorp.co.jp>
2018-04-12 11:49:58 +0900

    ログインフォームとバリデーション作成

--- app/action/Event/Login/Execute.php
+++ app/action/Event/Login/Execute.php
@@ -9,6 +9,12 @@
 class Yeahcheese_Form_EventLoginExecute extends Yeahcheese_ActionForm
 {
     public $form = [
+        'password' => [
+            'type'      =>  VAR_TYPE_STRING,
+            'form_type' =>  FORM_TYPE_PASSWORD,
+            'name'      =>  '認証キー',
+            'required'  =>  true,
+        ],
     ];
 }
 
@@ -16,6 +22,10 @@ class Yeahcheese_Action_EventLoginExecute extends Yeahcheese_ActionClass
 {
     public function prepare()
     {
+        if ($this->action_form->validate() > 0) {
+            return 'event_login';
+        }
+
         return null;
     }
 
--- template/ja_JP/event/login.tpl
+++ template/ja_JP/event/login.tpl
@@ -1,2 +1,16 @@
 <h2>イベント閲覧ログイン</h2>
 
+{foreach from=$errors item=error name=errors}
+    {if $smarty.foreach.errors.first}
+        <ul>
+    {/if}
+    <li>{$error}</li>
+    {if $smarty.foreach.errors.last}
+        </ul>
+    {/if}
+{/foreach}
+
+{form ethna_action="event_login_execute"}
+{form_name name="password"}{form_input name="password"}
+{form_submit value="ログイン"}
+{/form}
