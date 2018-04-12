9362cef27e173182ad7b6daa9d42e08c9f785b6d
mikankari <endo@sencorp.co.jp>
2018-04-12 11:43:01 +0900

    空の閲覧者ログインページを作成

---
+++ app/action/Event/Login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Event/Login.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Action_EventLogin extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return 'event_login';
+    }
+}
---
+++ app/action/Event/Login/Execute.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Event/Login/Execute.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Form_EventLoginExecute extends Yeahcheese_ActionForm
+{
+    public $form = [
+    ];
+}
+
+class Yeahcheese_Action_EventLoginExecute extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
