c88d7b41b32c6d09c5da2a4587d6c6e662025d0f
mikankari <endo@sencorp.co.jp>
2018-04-12 16:21:00 +0900

    共有者ロジックに、ログインメソッドを追加

--- app/Yeahcheese_UserManager.php
+++ app/Yeahcheese_UserManager.php
@@ -8,4 +8,27 @@
 
 class Yeahcheese_UserManager extends Ethna_AppManager
 {
+    /**
+     *  共有者がイベント管理のために認証する
+     *
+     *  @param  string  $email      共有者のメールアドレス
+     *  @param  string  $password   共有者のパスワード
+     *  @return                     共有者のID
+     */
+    public function login($email, $password): int
+    {
+        $userId = $this->db->getOne('SELECT id FROM users WHERE email = ? AND password = ?', [
+            $email,
+            hash('sha256', $password),
+        ]);
+
+        if (! $userId) {
+            return false;
+        }
+
+        $this->session->start();
+        $this->session->set('user_id', $userId);
+
+        return $userId;
+    }
 }
