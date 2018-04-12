59d3d228c17485849ba4861ef2bc2a638de20652
mikankari <endo@sencorp.co.jp>
2018-04-12 16:47:25 +0900

    空のログアウトアクションを追加

---
+++ app/action/User/Login/Revoke.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  User/Login/Revoke.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Action_UserLoginRevoke extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
