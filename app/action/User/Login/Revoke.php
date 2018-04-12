207f95f1ab4502f850030c975eba113e828952b9
mikankari <endo@sencorp.co.jp>
2018-04-12 16:48:24 +0900

    共有者ログアウト

--- app/action/User/Login/Revoke.php
+++ app/action/User/Login/Revoke.php
@@ -15,6 +15,11 @@ class Yeahcheese_Action_UserLoginRevoke extends Yeahcheese_ActionClass
 
     public function perform()
     {
+        $userManager = $this->backend->getManager('user');
+        $userManager->logout();
+
+        header('Location: ?action_user_login=true');
+
         return null;
     }
 }
--- template/ja_JP/user/index.tpl
+++ template/ja_JP/user/index.tpl
@@ -1,2 +1,4 @@
 <h2>ようこそ</h2>
 {$app.email} さん
+
+<a href="?action_user_login_revoke=true">ログアウト</a>
