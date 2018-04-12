c787bec2aedbf32311e13c3aa00417011fc87398
mikankari <endo@sencorp.co.jp>
2018-04-12 15:50:00 +0900

    ログインフォームの表示とバリデーション

--- app/action/User/Login/Execute.php
+++ app/action/User/Login/Execute.php
@@ -9,7 +9,18 @@
 class Yeahcheese_Form_UserLoginExecute extends Yeahcheese_ActionForm
 {
     public $form = [
-        
+        'email' => [
+            'name'      => 'メールアドレス',
+            'type'      => VAR_TYPE_STRING,
+            'required'  => true,
+            'custom'    => 'checkMailaddress',
+        ],
+        'password' => [
+            'name'      => 'パスワード',
+            'type'      => VAR_TYPE_STRING,
+            'form_type' => FORM_TYPE_PASSWORD,
+            'required'  => true,
+        ],
     ];
 }
 
@@ -17,6 +28,10 @@ class Yeahcheese_Action_UserLoginExecute extends Yeahcheese_ActionClass
 {
     public function prepare()
     {
+        if ($this->action_form->validate() > 0) {
+            return 'user_login';
+        }
+
         return null;
     }
 
--- template/ja_JP/user/login.tpl
+++ template/ja_JP/user/login.tpl
@@ -1,1 +1,17 @@
 <h2>ログイン</h2>
+
+{foreach from=$errors item=error name=errors}
+    {if $smarty.foreach.errors.first}
+        <ul>
+    {/if}
+    <li>{$error}</li>
+    {if $smarty.foreach.errors.last}
+        </ul>
+    {/if}
+{/foreach}
+
+{form ethna_action="user_login_execute"}
+{form_name name="email"}{form_input name="email"}
+{form_name name="password"}{form_input name="password"}
+{form_submit value="ログイン"}
+{/form}
