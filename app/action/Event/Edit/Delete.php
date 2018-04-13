dd92f053d6181af9549ba1605c25f5a88fb0a366
mikankari <endo@sencorp.co.jp>
2018-04-13 15:41:03 +0900

    空の写真削除アクションを作成

---
+++ app/action/Event/Edit/Delete.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Event/Edit/Delete.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Form_EventEditDelete extends Yeahcheese_ActionForm
+{
+    public $form = [
+
+    ];
+}
+
+class Yeahcheese_Action_EventEditDelete extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
