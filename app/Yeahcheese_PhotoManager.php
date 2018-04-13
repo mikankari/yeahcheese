7480e570b6a908952d1708da74a1770376f18c3b
mikankari <endo@sencorp.co.jp>
2018-04-13 16:13:01 +0900

    【修正】写真ロジックも型宣言に合わせる

--- app/Yeahcheese_PhotoManager.php
+++ app/Yeahcheese_PhotoManager.php
@@ -32,16 +32,16 @@ class Yeahcheese_PhotoManager extends Ethna_AppManager
      *
      *  @param  int     $eventId    対象とするイベントのID
      *  @param  string  $tmp_name   サーバが一時的にアップロードしたファイルのパス
-     *  @return int                 追加した写真のID。失敗した場合はFALSE。
+     *  @return int                 追加した写真のID。失敗した場合は 0
      */
     public function addEventPhoto(int $eventId, string $tmpName): int
     {
-        $result = $this->db->query('INSERT INTO photos (event_id) VALUES (?)', [
+        $result = $this->db->execute('INSERT INTO photos (event_id) VALUES (?)', [
             $eventId,
         ]);
 
         if (! $result) {
-            return false;
+            return 0;
         }
 
         $insertId = $this->db->getOne('SELECT max(id) FROM photos');
