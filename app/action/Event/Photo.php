745f0146c2da8a134b341c19946a55191ba6bf8d
mikankari <endo@sencorp.co.jp>
2018-04-23 16:16:53 +0900

    空の写真取得アクションを作成

---
+++ app/action/Event/Photo.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Event/Photo.php
+ *
+ *  @author     {$author}
+ *  @package    Yeahcheese
+ */
+
+class Yeahcheese_Form_EventPhoto extends Yeahcheese_ActionForm
+{
+    public $form = [
+
+    ];
+}
+
+class Yeahcheese_Action_EventPhoto extends Yeahcheese_ActionClass
+{
+    public function prepare()
+    {
+        return null;
+    }
+
+    public function perform()
+    {
+        return null;
+    }
+}
