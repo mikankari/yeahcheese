1b2ca7532abb0f73b5a4bfc2d9ff883315e79b1a
mikankari <endo@sencorp.co.jp>
2018-04-24 09:56:45 +0900

    ３回ループを１回にまとめる

--- app/action/Event/List.php
+++ app/action/Event/List.php
@@ -20,27 +20,18 @@ class Yeahcheese_Action_EventList extends Yeahcheese_ActionClass
 
         $this->action_form->setApp('events', $events);
 
+        $photoManager = $this->backend->getManager('photo');
         $publishAtText = [];
+        $statusLabel = [];
+        $photosCount = [];
         foreach ($events as $item) {
             $publishAtText[$item['id']] = Yeahcheese_EventManager::getPublishAtText($this->user['id'], $item['publish_start_at'], $item['publish_end_at']);
+            $statusLabel[$item['id']] = Yeahcheese_EventManager::getStatusLabel($item['publish_start_at'], $item['publish_end_at']);
+            $photosCount[$item['id']] = count($photoManager->getEventPhotos($item['id']));
         }
 
         $this->action_form->setAppNE('publishAtText', $publishAtText);
-
-        $statusLabel = [];
-        foreach ($events as $item) {
-            $statusLabel[$item['id']] = Yeahcheese_EventManager::getStatusLabel($item['publish_start_at'], $item['publish_end_at']);
-        }
         $this->action_form->setAppNE('statusLabel', $statusLabel);
-
-        $photosCount = [];
-        foreach ($events as $item) {
-            $photoManager = $this->backend->getManager('photo');
-            $photos = $photoManager->getEventPhotos($item['id']);
-
-            $photosCount[$item['id']] = count($photos);
-        }
-
         $this->action_form->setApp('photosCount', $photosCount);
 
         return 'event_list';
